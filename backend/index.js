d8a4c3ecc76daf58fca66a9054c867c7ee8a1a8f
unknown <visualanimal@icloud.com>
2024-04-12 02:35:49 +0800

    整合前端代码

--- backend/index.js
+++ backend/index.js
@@ -13,15 +13,15 @@ app.use(express.json());
 
 // Middleware for handling CORS POLICY
 // Option 1: Allow All Origins with Default of cors(*)
-// app.use(cors());
+app.use(cors());
 // Option 2: Allow Custom Origins
-app.use(
-  cors({
-    origin: 'chai.nfiii.cn:8080',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type'],
-  })
-);
+// app.use(
+//   cors({
+//     origin: 'chai.nfiii.cn:8080',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
 
 app.get('/', (request, response) => {
   console.log(request);
--- backend/routes/quoteRoute.js
+++ backend/routes/quoteRoute.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // GET all quotes
 router.get('/', async (req, res) => {
   try {
-    const quotes = await Quote.find();
+    const quotes = await Quote.find().sort('-updatedAt');
     res.json(quotes);
   } catch (err) {
     res.status(500).json({ message: err.message });
